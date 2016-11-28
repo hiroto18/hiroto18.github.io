660544b2eb8ce9c6238b83bf87c9e6321deb3960
Shoumma Shams <shoumma@gmail.com>
2016-11-28 01:04:30 -0800

    Added splashscreen
    Animations as well

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
 	
-	// Fade in and slide up
+	// Splash button events
 	
-    $(".hidden.info").fadeIn(2000);
-	$(".hidden.info").slideUp();
-	$(".hidden#arrow").delay(500).fadeIn(2000);
-	$(".hidden#arrow").delay(500).slideUp();
+	$("#splashbutton").hover(function() {
+        $(this)
+    });
+	
+	$("#splashbutton").click(function(){
+		
+		$("#splash").fadeOut(400, "swing");
+	
+		// Fade in and slide up	main page
+		$(".profileimg").delay(200).fadeIn(100);
+    	$(".hidden.info").delay(300).animate(
+			{opacity: "1", top:"10%"}, {duration: "slow"}, "swing");
+		$(".hidden#arrow").delay(500).animate(
+			{opacity: "1", bottom:"5%"}, {duration: "slow"},"swing");
+	
+	})
 	
 	// Pulse
 	
