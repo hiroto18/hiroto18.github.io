cc75b678cf232ab820ed91802a3486c133a38e44
Shoumma Shams <shoumma@gmail.com>
2016-11-29 03:55:08 -0800

    Added summary section
    Arrows css split, animation fixes

--- assets/js/js.js
+++ assets/js/js.js
@@ -16,14 +16,31 @@ $(document).ready(function(){
 		$(".profileimg").delay(300).fadeIn(100);
     	$(".info").delay(550).animate(
 			{opacity: "1", top:"20%"}, {duration: "slow"}, "swing");
-		$("#arrow").delay(750).animate(
+		$("#mainarrow").delay(750).animate(
 			{opacity: "1", bottom:"5%"}, {duration: "slow"},"swing");
-		$("#arrowshadow").delay(750).animate(
+		$("#mainarrowshadow").delay(750).animate(
 			{opacity: "1", bottom:"5%"}, {duration: "slow"},"swing");
-		$("#arrowshadow").attr("display", "block");
+		$("#mainarrowshadow").attr("display", "block");
 	})
 	
+	// Arrow scroll
 	
+	$("#mainarrow").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#summary").offset().top}, 1500);});
+	$("#summaryarrowup").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#intro").offset().top}, 1000);});
+	$("#summaryarrowdown").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#policyoverview").offset().top}, 1500);});
+	
+	// Summary page
+	
+		//Logos moves in
+		$("#mainarrow").click(function(){
+			$("#clintonlogo").delay(500).attr("left", "200px")});
+			$("#trumplogo").delay(500).attr("right", "-350px");
 	
 	
 });
