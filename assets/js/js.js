ff1e29a73dc77a8c2cb4ae6a310cce59b861ddfa
Shoumma Shams <shoumma@gmail.com>
2016-11-29 22:07:09 -0800

    Many changes

--- assets/js/js.js
+++ assets/js/js.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 	
 	$("#splashcontents").hide().fadeIn(1000);
 	
+	
+	
 	// Splash button events
 	
 	$("#splashbutton").hover(function() {
@@ -13,6 +15,8 @@ $(document).ready(function(){
 	$("#splashbutton").click(function(){
 		
 		$("#splash").fadeOut(400, "swing");
+	
+	
 	
 		// Fade in and slide up	main page
 		$(".profileimg").delay(300).fadeIn(100);
@@ -25,11 +29,19 @@ $(document).ready(function(){
 		$("#mainarrowshadow").attr("display", "block");
 	})
 	
-	// Arrow scroll
 	
-	$("#mainarrow").click(function() {
-    	$('html, body').animate({
-        	scrollTop: $("#summary").offset().top}, 1500);});
+	
+	// Navmenu
+	
+	$("#navicon").click(function() {
+		$(".navmenu").attr("height", "500px")});
+	$("#closenav").click(function() {
+		$(".navmenu").attr("height", "0")});
+	
+	
+	
+	// Arrow scroll
+
 	$("#summaryarrowup").click(function() {
     	$('html, body').animate({
         	scrollTop: $("#intro").offset().top}, 1000);});
@@ -40,25 +52,37 @@ $(document).ready(function(){
     	$('html, body').animate({
         	scrollTop: $("#summary").offset().top}, 1000);});
 	
-	// Summary page
+	
+	
+	// To summary page
+	
+	
 	
 		//Logos and arrows move in
 		$("#mainarrow").click(function(){
+			$('html, body').animate({
+        	scrollTop: $("#summary").offset().top}, 1500);
+			$("#navicon").delay(1500).animate(
+			{left: "3%", right: "97%"}, "slow");
 			$("#clintonlogo").delay(1750).attr("left", "200px");
 			$("#trumplogo").delay(1750).attr("right", "-350px");
-		$("#summaryarrowdown").delay(2000).animate(
+			$("#summaryarrowdown").delay(2000).animate(
 			{opacity: "1", bottom:"5%"}, {duration: "slow"},"swing");
-		$("#summaryarrowup").delay(2000).animate(
+			$("#summaryarrowup").delay(2000).animate(
 			{opacity: "1", top:"5%"}, {duration: "slow"},"swing")});
 			
 			
-	// Policy overview page
+			
+			
+	// To policy overview page
 	$("#summaryarrowdown").click(function(){
 		$("#policyarrowup").delay(2000).animate(
 			{opacity: "1", top:"5%"}, {duration: "slow"},"swing")
 		$("#policytexttitle").delay(1750).fadeIn(100);
 		$("#policysub").delay(1875).fadeIn(100);
 		$("#policyoverview").delay(1950).fadeIn(100);});
+		
+		
 		
 	// Policy page links
 	
@@ -82,22 +106,37 @@ $(document).ready(function(){
 			$("#socialarrowup").delay(1500).animate(
 			{opacity: "1", top:"5%"}, {duration: "slow"},"swing");});
 			
+			
+			
 	// Policy arrows
 	
 	$("#environmentarrowdown").click(function() {
     	$('html, body').animate({
-        	scrollTop: $("#policyeconomy").offset().top}, 1000);});
+        	scrollTop: $("#policyeconomy").offset().top}, 1000);
+		$("#economyarrowup").delay(1500).animate(
+			{opacity: "1", top:"5%"}, {duration: "slow"},"swing");
+		$("#economyarrowdown").delay(1500).animate(
+			{opacity: "1", bottom:"5%"}, {duration: "slow"},"swing");});
 			
 	$("#economyarrowup").click(function() {
     	$('html, body').animate({
-        	scrollTop: $("#policyenvironment").offset().top}, 1000);});
+        	scrollTop: $("#policyenvironment").offset().top}, 1000);
+		$("#environmentarrowdown").delay(1500).animate(
+			{opacity: "1", bottom:"5%"}, {duration: "slow"},"swing");});
+			
 	$("#economyarrowdown").click(function() {
     	$('html, body').animate({
-        	scrollTop: $("#policysocial").offset().top}, 1000);});
+        	scrollTop: $("#policysocial").offset().top}, 1000);
+		$("#socialarrowup").delay(1500).animate(
+			{opacity: "1", top:"5%"}, {duration: "slow"},"swing");});
 			
 	$("#socialarrowup").click(function() {
     	$('html, body').animate({
-        	scrollTop: $("#policyeconomy").offset().top}, 1000);});
+        	scrollTop: $("#policyeconomy").offset().top}, 1000);
+		$("#economyarrowup").delay(1500).animate(
+			{opacity: "1", top:"5%"}, {duration: "slow"},"swing");
+		$("#economyarrowdown").delay(1500).animate(
+			{opacity: "1", bottom:"5%"}, {duration: "slow"},"swing");});
 		
 	
 	
